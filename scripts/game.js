4bc14b05161f42013779e2643aa1d1f599b2b621
Trequend <68006090+Trequend@users.noreply.github.com>
2020-07-22 01:56:18 +0300

    Added Input class

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,6 @@
 import GameObject from './core/GameObject.js';
 import Screen from './core/Screen.js';
+import Input from './core/Input.js';
 
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
@@ -18,9 +19,12 @@ let deltaTime = 0;
 let lastFrameTime = performance.now();
 
 Screen.initialize(canvas);
+Input.initialize();
 
 const loop = () => {
 	deltaTime += Math.min(0.4, (performance.now() - lastFrameTime) / 1000);
+
+	Input.process();
 
 	while (deltaTime > step) {
 		deltaTime -= step;
