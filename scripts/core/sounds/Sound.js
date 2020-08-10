f08833e089a61aabe00cedb995feb0234773ec5d
caapricorn <caapricorn@mail.ru>
2020-08-10 04:53:28 +0300

    Added spatial sound and other

--- scripts/core/sounds/Sound.js
+++ scripts/core/sounds/Sound.js
@@ -2,7 +2,12 @@ export default class Sound {
 	constructor(buffer) {
 		this.buffer = buffer;
 	}
-
+	
+	/**
+	 * @param {AudioContext} context
+	 * 
+	 * @return {AudioBufferSourceNode} Создает источник звука из буфера.
+	 */
 	createSourceNode(context) {
 		const sourceNode = context.createBufferSource();
 		sourceNode.buffer = this.buffer;
